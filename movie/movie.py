a074367f73a3120ef6f98e4fa7c142e8d8bf8ac1
Antoine CHEUCLE <antoine.cheucle@imt-atlantique.net>
2024-09-25 09:48:59 +0200

    init: TP après tuto

--- movie/movie.py
+++ movie/movie.py
@@ -7,7 +7,18 @@ PORT = 3001
 HOST = '0.0.0.0'
 app = Flask(__name__)
 
-# todo create elements for Ariadne
+type_defs = load_schema_from_path('movie.graphql')
+query = QueryType()
+movie = ObjectType('Movie')
+query.set_field('movie_with_id', r.movie_with_id)
+
+mutation = MutationType()
+mutation.set_field('update_movie_rate', r.update_movie_rate)
+
+actor = ObjectType('Actor')
+movie.set_field('actors', r.resolve_actors_in_movie)
+
+schema = make_executable_schema(type_defs, movie, query, mutation, actor)
 
 # root message
 @app.route("/", methods=['GET'])
@@ -17,7 +28,15 @@ def home():
 # graphql entry points
 @app.route('/graphql', methods=['POST'])
 def graphql_server():
-    # todo to complete
+    data = request.get_json()
+    success, result = graphql_sync(
+                        schema,
+                        data,
+                        context_value=None,
+                        debug=app.debug
+                    )
+    status_code = 200 if success else 400
+    return jsonify(result), status_code
 
 if __name__ == "__main__":
     print("Server running in port %s"%(PORT))
