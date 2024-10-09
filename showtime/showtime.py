840b09eddd490773647444f90536ddc757967abc
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2024-10-09 09:04:28 +0200

    add showtime methodes code

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -10,11 +10,42 @@ class ShowtimeServicer(showtime_pb2_grpc.ShowtimeServicer):
         with open('{}/data/times.json'.format("."), "r") as jsf:
             self.db = json.load(jsf)["schedule"]
 
+    # write the code for the rpc call
+
+    def Showtimes(self, request, context):
+        print("Showtimes")
+        showtimes_data = showtime_pb2.ShowtimesData(
+            showtimes=[
+                showtime_pb2.ShowtimeData(
+                    date=showtime['date'],
+                    movies=showtime['movies']
+                ) for showtime in self.db
+            ]
+        )
+        return showtimes_data
+    
+    def GetMovieByDate(self, request, context):
+        print("GetMovieByDate")
+        date = request.date
+        # Filter for the requested date
+        movie = next((showtime for showtime in self.db if showtime["date"] == date), None)
+        
+        if movie:
+            return showtime_pb2.ShowtimeData(
+                date=movie["date"],
+                movies=movie["movies"]
+            )
+        else:
+            context.set_code(grpc.StatusCode.NOT_FOUND)
+            context.set_details(f"No movies found for the date: {date}")
+            return showtime_pb2.ShowtimeData()
+
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     showtime_pb2_grpc.add_ShowtimeServicer_to_server(ShowtimeServicer(), server)
     server.add_insecure_port('[::]:3002')
     server.start()
+    print("Server running in port 3002")
     server.wait_for_termination()
 
 
