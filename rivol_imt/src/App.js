fe966d8307ca06cb50abf633ad94d755d920045b
nathan-claeys <127297857+nathan-claeys@users.noreply.github.com>
2024-10-16 11:57:19 +0200

    Refactor App.js to add new routes for Home and 404 page components

--- rivol_imt/src/App.js
+++ rivol_imt/src/App.js
@@ -2,12 +2,16 @@ import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Bookings from "./routes/Bookings";
+import Home from "./routes/Home";
+import Page404 from "./routes/Page404";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/bookings" element={<Bookings date={"20151202"}/>} />
+                <Route path="/" element={<Home />} />
+                <Route path="*" element={<Page404 />} />
             </Routes>
         </BrowserRouter>
     );
