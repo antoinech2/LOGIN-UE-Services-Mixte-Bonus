2096b31b23e0af857be1352fed7a32e2e9a9ffc2
Phoenixcresset <jeanbaptiste.lambertin@gmail.com>
2024-10-20 17:07:23 +0200

    Feat : Added gRPC proto compiling to sh file

--- init.sh
+++ init.sh
@@ -8,3 +8,16 @@ python booking/database/database.py
 python movie/database/database.py
 python showtime/database/database.py
 python user/database/database.py
+
+# Compile gRPC proto files
+cd booking
+python -m grpc_tools.protoc -I=./protos --python_out=. --grpc_python_out=. booking.proto
+python -m grpc_tools.protoc -I=./protos --python_out=. --grpc_python_out=. showtime.proto
+cd ..
+cd showtime
+python -m grpc_tools.protoc -I=./protos --python_out=. --grpc_python_out=. showtime.proto
+cd ..
+cd user
+python -m grpc_tools.protoc -I=./protos --python_out=. --grpc_python_out=. booking.proto
+python -m grpc_tools.protoc -I=./protos --python_out=. --grpc_python_out=. showtime.proto
+cd ..
